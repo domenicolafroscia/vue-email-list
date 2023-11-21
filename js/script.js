cafdc7d67eacd7e9b4bf2d0a6a469e6968c99a67
domenicolafroscia <domenicolafroscia@gmail.com>
2023-11-21 15:26:00 +0100

    add: creazione lista di 10 email

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            randomEmail: [],
+        }
+    },
+    methods: {
+        generateEmail() {
+            for (let i = 0; i < 10; i++){
+                axios
+                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((resp) => {
+                        this.randomEmail.push(resp.data.response)                  
+                    })
+            }
+        }
+    }
+}).mount("#app");
+
 
 
 
